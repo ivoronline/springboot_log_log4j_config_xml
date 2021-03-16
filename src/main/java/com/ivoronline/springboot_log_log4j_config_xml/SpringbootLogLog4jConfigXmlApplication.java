186f6bbc5c415802dbfd01f2750972efcafa4f34
igoreta <ivoronline@gmail.com>
2021-03-16 23:02:56 +0100

    Initial commit

---
+++ src/main/java/com/ivoronline/springboot_log_log4j_config_xml/SpringbootLogLog4jConfigXmlApplication.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_log_log4j_config_xml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLogLog4jConfigXmlApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootLogLog4jConfigXmlApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ivoronline/springboot_log_log4j_config_xml/SpringbootLogLog4jConfigXmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_log_log4j_config_xml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLogLog4jConfigXmlApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
